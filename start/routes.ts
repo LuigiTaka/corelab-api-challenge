4bbb431377a2ca9988df1ef117c966562d9cfa63
Luigi <luigimendeszanchett@gmail.com>
2024-04-02 19:03:15 -0300

    ✨ Adiciona rotas para manipular os registros Todo

--- start/routes.ts
+++ start/routes.ts
@@ -20,4 +20,10 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/vehicles', 'VehiclesController.index');
+Route.get("/todos",'TodosController.index')
+Route.post("/todos",'TodosController.post')
+
+Route.get("/todos/:id",'TodosController.index')
+Route.put("/todos/:id",'TodosController.put')
+Route.patch("/todos/:id",'TodosController.patch')
+Route.delete("/todos/:id",'TodosController.delete')
