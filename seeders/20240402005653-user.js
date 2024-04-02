088b51beb260ac676c84c0f24064a79c8d90730f
Luigi <luigimendeszanchett@gmail.com>
2024-04-01 22:53:51 -0300

    🗃️ Cria migração para tabela de usuarios
    Adiciona model e seeder para a tabela.

---
+++ seeders/20240402005653-user.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', [{
+      firstName: "Luigi",
+      lastName: "zanchett",
+      email: "random@gmail.com",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
